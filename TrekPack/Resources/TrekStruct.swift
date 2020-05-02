f7a331d7d04777d22a33ca60182aa7c27a34803a
Tobias Moktar <moktart@sheridancollege.ca>
2020-05-02 17:27:11 -0400

    Modified TrekClass so its a struct

---
+++ TrekPack/Resources/TrekStruct.swift
@@ -0,0 +1,20 @@
+//
+//  TrekStruct.swift
+//  TrekPack
+//
+//  Created by Toby moktar on 2020-01-05.
+//  Copyright © 2020 Moktar. All rights reserved.
+//
+
+import UIKit
+
+
+struct Trek {
+    var name: String
+    var destination: String
+    var departureDate: String
+    var returnDate: String
+    var items: [String]
+    var tags: [String]
+    
+}
